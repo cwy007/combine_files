cd5674161ea3bdb01a1419e46f79075737092999
chenweiyan <chanweiyan007@gmail.com>
2019-08-06 14:49:40 +0800

    拆分文件

--- combine_files.rb
+++ combine_files.rb
@@ -1,5 +1,6 @@
 require "json"
 
+# 合并文件
 tests = []
 Dir["source/**/**.side"].each do |file_path|
   test_case = JSON.parse(File.read(file_path))
@@ -12,3 +13,16 @@ udesk_atar["tests"] = tests.flatten.uniq
 File.open("udesk_atar.side", "w") { |f| f.write udesk_atar.to_json }
 
 puts `cat udesk_atar.side`.split(",")
+
+# 拆分文件
+udesk_atar = JSON.parse(File.read("udesk_atar.side"))
+puts udesk_atar["tests"].count
+
+udesk_atar["tests"].each do |test|
+  file_path = "source/#{test["name"]}.side"
+  next if File.file?(file_path)
+  test_case = udesk_atar
+  test_case["tests"] = [test]
+
+  File.open("#{file_path}", "w") { |f| f.write test_case.to_json }
+end
